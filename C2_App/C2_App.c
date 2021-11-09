7dc8bd6b6bc5658ba44a5e5fa3f9ae50375f17d0
youtube-programmercpp <sbl908k@yahoo.co.jp>
2021-11-09 21:48:02 +0900

    ライブ終了時点のコード

---
+++ C2_App/C2_App.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+void f1()
+{
+}
+int f2()
+{
+	return 1;
+}
+void f3()
+{
+}
+void f4()
+{
+}
+int main()
+{
+	for (f1(); f2(); f3())
+		f4();
+}
---
+++ C_App/C_App.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+int main()
+{
+	for (int i = 0;   ;   ) {
+		static const char s[] = "ABC全角ﾊﾝｶｸｶﾀｶﾅひらがな";
+		//データ定義
+		if (s[i] == '\0')
+			break;
+		else if (IsDBCSLeadByte(s[i])) {
+			//全角
+			putchar(s[i++]);
+			putchar(s[i++]);
+			putchar('\n');
+		}
+		else {
+			//半角
+			putchar(s[i++]);
+			putchar('\n');
+		}
+	}
+}
